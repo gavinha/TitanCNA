90b5ee5bdb427b763760c5792385f7882a994b47
Gavin Ha <gavinha@broadinstitute.org>
2017-05-21 19:20:40 -0400

    use pysam instead of jointpysam

--- scripts/TenX_scripts/getTumourAlleleCountsAtHETSites.py
+++ scripts/TenX_scripts/getTumourAlleleCountsAtHETSites.py
@@ -28,56 +28,56 @@ for line in open(norm_hetpsns):
                 positions[chrom] = []
             positions[chrom].append(position_data)
 
-sample = jointpysam.bam.BamFile(bam_file)
-reference = jointpysam.fasta.FastaFile(ref_file)
+sample = pysam.AlignmentFile(bam_file)
+#reference = pysam.FastaFile(ref_file)
 ## print header ##
 print "Chr\tPosition\tRef\tRefCount\tNref\tNrefCount\tNormQuality\tNormGenotype\tNormPhaseSet"
 
 for chrom in positions:
     i = 0
     for position_data in positions[chrom]:
-        position = int(position_data[0])
-        result = str(chrom) + "\t" + str(position)
-        ref_base = position_data[2]
-        nref_base = position_data[3]
-        qual = float(position_data[4])
-        genoKey = position_data[5]
-        geno = position_data[6]
-        #print base,
-        if qual >= vcf_quality and qual != None:
-            _p = sample.get_pileup_iterator(chrom,position,position + 1)
-            pileup = None
-            for p in _p:
-                if p.position == position:
-                    pileup = p
-                
-            if pileup is None:
-                result += "\t" + ref_base + "\t0\t" + nref_base + "\t0" + "\t" + str(qual)
-                #print result
-                continue
-        
-            ref_count = 0
-            depth = 0
-            bases = filter((lambda base: base['base_qual'] >= base_quality and base['map_qual'] >= map_quality), pileup.base_info)
-        
-            for base in bases:
-              depth += 1
-              if base["base"] == ref_base:
-                ref_count += 1
-            alt_count = depth - ref_count
-            gt_ind = genoKey.index("GT")
-            ps_ind = None
-            try:
-                ps_ind = genoKey.index("PS")
-            except ValueError:
-                sys.stdout.write('%s is not in a phase set.' % result)
-            if ps_ind is None:
-                ps_val = "NA"
-            else:
-                ps_val = str(geno[ps_ind])
-            #print str(depth) + "\t" + str(ref_count) + "\t" + str(alt_count)
-            if (int(ref_count) + int(alt_count)) > 0:
-                result += "\t" + ref_base + "\t" + str(ref_count) + "\t" + nref_base + "\t" + str(alt_count) + '\t' + str(qual) + '\t' + str(geno[gt_ind]) + '\t' + ps_val
-                print result
-                i += 1
+			position = int(position_data[0])
+			result = str(chrom) + "\t" + str(position)
+			ref_base = position_data[2]
+			nref_base = position_data[3]
+			qual = float(position_data[4])
+			genoKey = position_data[5]
+			geno = position_data[6]
+			#print base,
+			if qual >= vcf_quality and qual != None:
+				_p = sample.pileup(reference=chrom, start=position, end=position + 1)
+				bases = list()
+				for p in _p:
+					if p.reference_pos == position:
+						for r in p.pileups:
+							if not r.is_del and not r.is_refskip:
+								base = r.alignment.query_sequence[r.query_position-1]
+								mapq = r.alignment.mapping_quality
+								baseq = r.alignment.query_qualities[r.query_position-1]
+								if mapq >= map_quality and baseq >= base_quality:
+									bases.append(base)
+			
+				ref_count = 0
+				depth = 0   
+				for base in bases:
+					depth += 1
+					if base == ref_base:
+						ref_count += 1
+				alt_count = depth - ref_count
+				gt_ind = genoKey.index("GT")
+				ps_ind = None
+				try:
+					ps_ind = genoKey.index("PS")
+				except ValueError:
+					sys.stdout.write('%s is not in a phase set.' % result)
+				if ps_ind is None:
+					ps_val = "NA"
+				else:
+					ps_val = str(geno[ps_ind])
+				#print str(depth) + "\t" + str(ref_count) + "\t" + str(alt_count)
+				#if (int(ref_count) + int(alt_count)) > 0:
+				result += "\t" + ref_base + "\t" + str(ref_count) + "\t" + nref_base + "\t" + str(alt_count) + '\t' + str(qual) + '\t' + str(geno[gt_ind]) + '\t' + ps_val
+				print result
+				i += 1
+						
         
